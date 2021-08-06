6e485484096eefa4212465ee3a2bf7564c285329
Shubham Karekar <66383444+SrK1080@users.noreply.github.com>
2021-08-06 13:37:21 +0530

    Update settings.py

--- basic/settings.py
+++ basic/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1gl+^0ew)0g*9hvhtdtk%b4$s!bg*e==v@+5k6zw)b#zzh1jup'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sklanguage.herokuapp.com']
 
