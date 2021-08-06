440a50da6e4db9de5555bfffd5bc9db33bdaaaea
Shubham Karekar <66383444+SrK1080@users.noreply.github.com>
2021-08-06 11:33:01 +0530

    Update urls.py

--- frizzer/urls.py
+++ frizzer/urls.py
@@ -4,5 +4,5 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('click',views.click,name='click'),
     path('show',views.show,name='show'),
-    path('info',views.info,name='info'),
+    
 ]
